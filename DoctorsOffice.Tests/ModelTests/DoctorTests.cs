806d9fed5d57b404182da4af618d04101030a59a
Austin Barr and Hisato Kawaminami <hisato@>
2018-07-17 10:45:15 -0700

    wrote two tests all passing, all methods working appropratly

--- DoctorsOffice.Tests/ModelTests/DoctorTests.cs
+++ DoctorsOffice.Tests/ModelTests/DoctorTests.cs
@@ -27,5 +27,49 @@ namespace DoctorsOffice.Tests
       //Assert
       Assert.AreEqual(0, result);
     }
+
+    [TestMethod]
+    public void CanWeCompareTwoDoctorswithSameinfo()
+    {
+      //Arrange/act
+      Doctor firstDoctor = new Doctor("Ai", "Brain Damage");
+      Doctor secondDoctor = new Doctor("Ai", "Brain Damage");
+
+      //Assert
+      Assert.AreEqual(firstDoctor, secondDoctor);
+    }
+
+    [TestMethod]
+    public void GetaListBackwithGETALL()
+    {
+      //Arrange
+      Doctor testDoctorOne = new Doctor("YU", "Family Doctor");
+      testDoctorOne.Save();
+      Doctor testDoctorTwo = new Doctor("Mi", "Family Doctor");
+      testDoctorTwo.Save();
+      //Act
+      List<Doctor> testlist = Doctor.GetAll();
+      List<Doctor> testlistTwo = new List<Doctor> {testDoctorOne, testDoctorTwo};
+
+      //Assert
+      CollectionAssert.AreEqual(testlist, testlistTwo);
+    }
+
+    [TestMethod]
+    public void Save_DatabaseAssignIdToCategory_id()
+    {
+      //Arrange
+      Doctor testDoctor = new Doctor("Hisato", "Cardiology");
+      testDoctor.Save();
+
+      //act
+      Doctor savedDoctor = Doctor.GetAll()[0];
+
+      int result = savedDoctor.GetId();
+      int testId = testDoctor.GetId();
+
+      //Assert
+      Assert.AreEqual(result, testId);
+    }
   }
 }
